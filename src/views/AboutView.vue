3e8a7a9d4101ef3d3196996b64b165e0287b3b03
JunyiF <junyifeng98@gmail.com>
2022-11-27 18:21:21 -0800

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,7 +8,13 @@
       <h1 class="text-5xl sm:text-6xl text-center">About Me.</h1>
     </div>
     <div class="my-8 m-6 sm:m-12 lg:px-36 ">
-      My name is Feng Junyi, I am from China. I moved to the United States in 2017 and living in Portland for 5 years. I graduated from David Douglas High School in 2019, and now I'm an undergraduate computer science student at Portland State University and expected to graduate in June 2023. I am interested in Software Engineering and Gaming Development. I am passionate about bringing the worlds I imagine to life. So I am always curious to learn more about new technologies and creative coding, and I also love learning about design. Movies are my greatest hobby.
+      My name is Junyi Feng, I am from China. I moved to the United States in 2017 
+      and living in Portland for 5 years. I graduated from David Douglas High School in 2019, 
+      and now I'm an undergraduate computer science student at Portland State University and expected 
+      to graduate in June 2023. I am interested in Software Engineering and Gaming Development. 
+      I am passionate about bringing the worlds I imagine to life. So I am always curious to learn 
+      more about new technologies and creative coding, and I also love learning about design. Movies 
+      are my greatest hobby.
     </div>
   </main>
 </template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+
+
 </script>
 
 <template>
@@ -7,7 +9,7 @@
     <h1 class="text-4xl text-center">Contact.</h1>
     <div class="py-8 lg:py-12 px-4 mx-auto max-w-screen-md">
       <p class="mb-8 lg:mb-16 text-center sm:text-xl ">Please feel free to leave feedback on your experience.</p>
-      <form action="#" class="space-y-8">
+      <form class="space-y-8">
           <div>
               <label for="name" class="block mb-2 text-gray-800">Your name</label>
               <input type="text" id="name" class="block p-3 w-full  text-black bg-gray-50 rounded-lg border border-gray-300 drop-shadow-xl" placeholder="Nice to meet you" required>
@@ -22,9 +24,21 @@
               <textarea id="message" rows="8" class="block p-3 w-full text-black bg-gray-50 rounded-lg border border-gray-300 drop-shadow-xl" placeholder="Leave a comment..."></textarea>
           </div>
           <div class="text-center">
-            <button type="submit" class="py-3 px-5 text-center text-white rounded-lg bg-gray-500 drop-shadow-xl hover:bg-gray-800">Send message</button>
+            <button @click="redirectToHome()" type="submit" id="app" class="py-3 px-5 text-center text-white rounded-lg bg-gray-500 drop-shadow-xl hover:bg-gray-800">Send message</button>
           </div>
       </form>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: "#app",
+  methods: {
+    redirectToHome() {
+      this.$router.push({ path: '/' });
+      alert("Thank You!")
+    },
+  }
+};
+</script>
