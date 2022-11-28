57b287d4a15bc0e12dc975b5263250b94a48ce79
JunyiF <junyifeng98@gmail.com>
2022-11-28 11:46:14 -0800

    commit project page update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/CS410-JunyiFeng-Website/',
+  base: '/CS410-JF-Website/',
   /*
   resolve: {
     alias: {
